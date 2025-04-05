5a6023fd0cd68841527dc6e00a1b6951a5a4c35f
Lee Seung Been <sudangi143@gmail.com>
2025-04-05 15:57:08 +0900

    Feat : PGM도 같이 저장

--- frame_utils.h
+++ frame_utils.h
@@ -7,6 +7,9 @@
 // 프레임 저장 함수
 int saveFrame(IplImage* frame, const char* outputDir, int frameCount);
 
+// 프레임 PGM 저장 함수
+int saveFrameAsPGM(IplImage* frame, const char* outputDir, int frameCount)
+
 // 프레임 전체 추출
 int extractFrames(CvCapture* capture, const char* outputDir);
 
