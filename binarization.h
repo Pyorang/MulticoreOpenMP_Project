72123cdc72b77d0e822c72878277d9364ec9354e
P13Xm4 <150218326+P13Xm4@users.noreply.github.com>
2025-04-09 05:25:22 +0900

    파일명 및 내용 수정을 위한 삭제

--- binarization.h
+++
@@ -1,9 +0,0 @@
-#ifndef BINARIZATION_H
-#define BINARIZATION_H
-
-#include <stdint.h>
-
-void compute_difference(uint8_t* img1, uint8_t* img2, uint8_t* diff, int width, int height);
-void binarize_image_parallel(uint8_t* diff, uint8_t* binary, int width, int height, uint8_t threshold);
-
-#endif
